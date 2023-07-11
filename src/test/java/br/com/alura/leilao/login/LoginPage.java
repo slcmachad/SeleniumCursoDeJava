e8e98af1d8f3d65abcbf3ee11c9e47b9cabfcb12
Salomao <salomao.machado.75@gmail.com>
2023-07-10 22:29:40 -0300

    <refactor> Criando classe para aplicar metodo Page Object Model.

---
+++ src/test/java/br/com/alura/leilao/login/LoginPage.java
@@ -0,0 +1,8 @@
+package br.com.alura.leilao.login;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	private static final String URL_LOGIN = "http://localhost:8080/login";
+	private WebDriver browser;
+}
--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -13,8 +13,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 	
-	private static final String URL_LOGIN = "http://localhost:8080/login";
-	private WebDriver browser;
+	private LoginPage paginaDeLogin;
+	
+	
 	
 	@BeforeAll
 	public static void beforeAll() {
