81ef4a8f7de55ed3a6b3f5fae0ddf490031e0cd1
Salomao <salomao.machado.75@gmail.com>
2023-07-11 10:07:23 -0300

    <build> Criação de classe para fazer testes na criação de leiloes

---
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -0,0 +1,56 @@
+package br.com.alura.leilao.leiloes;
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LeiloesTest {
+	
+	private LeiloesPage paginaDeLeiloes;
+	
+	@BeforeEach
+	public void beforeEach() {
+		this.paginaDeLogin = new LoginPage();
+	}
+	
+	@AfterEach
+	public void afterEach() {
+		this.paginaDeLogin.fechar();
+	}
+
+	@Test
+	public void deveriaEfetuarLoginComDadosValidos() {
+		paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
+		paginaDeLogin.efetuaLogin();
+
+		Assert.assertFalse(paginaDeLogin.isPaginaDeLogin());
+		Assert.assertEquals("fulano", paginaDeLogin.getNomeUsuarioLogado());
+	}
+	
+	@Test
+	public void naoDeveriaLogarComDadosInvalidos() {
+		paginaDeLogin.preencheFormularioDeLogin("invalido", "123456");
+		paginaDeLogin.efetuaLogin();
+		
+		Assert.assertTrue(paginaDeLogin.isPaginaDeLoginComDadosInvalidos());
+		Assert.assertNull(paginaDeLogin.getNomeUsuarioLogado());
+		Assert.assertFalse(paginaDeLogin.contemTexto("Usuário e senha Inválidos."));
+
+	}
+	
+	@Test
+	public void naoDeveriaAcessarPaginaRestritaSemEstarLogado() {
+		paginaDeLogin.navegaParaPaginaDeLances();
+
+		Assert.assertTrue(paginaDeLogin.isPaginaDeLogin());
+		Assert.assertFalse(paginaDeLogin.contemTexto("Dados do Leilão"));
+
+	}
+}
