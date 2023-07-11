0742f3b4655c4aada7e1c48338c571f651c42471
Salomao <salomao.machado.75@gmail.com>
2023-07-10 21:56:30 -0300

    <feat> Teste para verificar quando login for invalido

--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.login;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,6 +20,21 @@ public class LoginTest {
 
 		Assert.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
 		Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
+		browser.quit();
+	}
+	
+	@Test
+	public void naoDeveriaLogarComDadosInvalidos() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:8080/login");
+		browser.findElement(By.id("username")).sendKeys("invalido");
+		browser.findElement(By.id("password")).sendKeys("123132");
+		browser.findElement(By.id("login-form")).submit();
+
+		Assert.assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login?error"));
+		Assert.assertTrue("fulano", browser.getPageSource().contains("Usuário e senha inválidos."));
+		Assert.assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")));
 		browser.quit();
 	}
 	
