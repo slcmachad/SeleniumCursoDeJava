c1d08a6667aabe53e867d62942f1c62b452cbd47
Salomao <salomao.machado.75@gmail.com>
2023-07-11 11:25:14 -0300

    <feat> Iniciando teste e fazendo login para excecutalos

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -1,28 +1,26 @@
 package br.com.alura.leilao.leiloes;
 
 
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import br.com.alura.leilao.login.LoginPage;
+
 
 public class LeiloesTest {
 	
 	private LeiloesPage paginaDeLeiloes;
-	
-	@BeforeEach
-	public void beforeEach() {
-		this.paginaDeLeiloes = new LeiloesPage();
-	}
-	
+		
 	@AfterEach
 	public void afterEach() {
 		this.paginaDeLeiloes.fechar();
+	}
+	
+	@Test
+	public void deveriaCadastrarLeilao() {
+		LoginPage paginaDeLogin = new LoginPage();
+		paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
+		paginaDeLogin.efetuaLogin();
 	}
 	
 }
