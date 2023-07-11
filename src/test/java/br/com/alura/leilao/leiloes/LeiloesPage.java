ae72c4c2629d67f07b00bffa0098136837ee0ba4
Salomao <salomao.machado.75@gmail.com>
2023-07-11 10:18:42 -0300

    <feat> Organizando imports e apagando metodos desnescessarios

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesPage.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesPage.java
@@ -1,57 +1,20 @@
 package br.com.alura.leilao.leiloes;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LeiloesPage {
-	private static final String URL_LOGIN = "http://localhost:8080/login";
+	private static final String URL_LEILOES = "http://localhost:8080/leiloes";
 	private WebDriver browser;
 	
 	public LeiloesPage() {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		this.browser = new ChromeDriver();
-		browser.navigate().to(URL_LOGIN);
+		browser.navigate().to(URL_LEILOES);
 	}
 
 	public void fechar() {
 
 		this.browser.quit();
 	}
-
-	public void preencheFormularioDeLogin(String username, String password) {
-
-		browser.findElement(By.id("username")).sendKeys(username);
-		browser.findElement(By.id("password")).sendKeys(password);
-	}
-
-	public void efetuaLogin() {
-		browser.findElement(By.id("login-form")).submit();		
-	}
-
-	public boolean isPaginaDeLogin() {
-		return browser.getCurrentUrl().equals(URL_LOGIN);
-	}
-
-	public String getNomeUsuarioLogado() {
-		try {
-			return  browser.findElement(By.id("usuario-logado")).getText();	
-		} catch (NoSuchElementException e) {
-			return null;
-		}
-	}
-
-	public void navegaParaPaginaDeLances() {
-		this.browser.navigate().to("http://localhost:8080/leiloes/2");
-	}
-
-	public boolean contemTexto(String texto) {
-		return browser.getPageSource().contains(texto);
-	}
-
-	public boolean isPaginaDeLoginComDadosInvalidos() {
-		return browser.getCurrentUrl().equals(URL_LOGIN + "?error");
-	}
-
 }
