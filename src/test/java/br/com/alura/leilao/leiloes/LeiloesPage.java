a9475aee0cce3c3dd108cbc6089af7e9c3e47023
Salomao <salomao.machado.75@gmail.com>
2023-07-12 16:35:16 -0300

    <feat> pegando elementos do CSS para validar testes

--- src/test/java/br/com/alura/leilao/leiloes/LeiloesPage.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesPage.java
@@ -1,6 +1,8 @@
 package br.com.alura.leilao.leiloes;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LeiloesPage {
 	private static final String URL_CADASTRO_LEILOES = "http://localhost:8080/leiloes/new";
@@ -20,7 +22,13 @@ public class LeiloesPage {
 		return new CadastroLeiloesPage(browser);
 	}
 
-	public boolean isLeilaoCadastrado(String nome, String valor, String hoje) {
-		return false;
+	public boolean isLeilaoCadastrado(String nome, String valorInicial, String dataAbertura) {
+		WebElement linhaDaTabela = this.browser.findElement(By.cssSelector("#tabela-leiloes tbody tr:last-child"));
+		WebElement colunaNome = linhaDaTabela.findElement(By.cssSelector("td:nth-child(1)"));
+		WebElement colunaDataAbertura = linhaDaTabela.findElement(By.cssSelector("td:nth-child(2)"));
+		WebElement colunaValorInicial = linhaDaTabela.findElement(By.cssSelector("td:nth-child(3)"));
+		return colunaNome.getText().equals(nome) 
+				&& colunaDataAbertura.getText().equals(dataAbertura) 
+				&& colunaValorInicial.getText().equals(valorInicial);
 	}
 }
